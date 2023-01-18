4ddd3f1f5116898fbe9eecc41ce7037f72642682
KellyMart8 <110624872+KellyMart8@users.noreply.github.com>
2023-01-17 23:53:38 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using ejercicio1;
+using System;
+
+ClassNumero cn = new ClassNumero();
+int suma;
+
+do
+{
+    cn.CEstu = cn.Validar("Ingrese la cantidad de estudiantes ");
+
+    for (int j = 0; j < 4; j++)
+    {
+        do
+        {
+            cn.Num = cn.Validar("Ingresa la " + (1 + j) + "° nota en el rango de 0 a 25: ");
+
+        } while (cn.Num < 1 || cn.Num > 25);
+
+            suma += cn.Num[i];
+            Console.WriteLine("El resultado de la sumatoria es:");
+
+        Console.WriteLine(cn.Estado);
+    }
+} while (cn.CEstu < 1 || cn.CEstu > 24);
+
